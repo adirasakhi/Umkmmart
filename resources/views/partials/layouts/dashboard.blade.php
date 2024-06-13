0630d3c319203aea89802b3d72737c589fdc8544
unknown <enungnurjanah685@gmail.com>
2024-06-13 14:25:31 +0700

    dashboard page

---
+++ resources/views/partials/layouts/dashboard.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>General Dashboard &mdash; Stisla</title>
+
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="{{ asset('assets/modules/bootstrap/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/modules/fontawesome/css/all.min.css') }}">
+
+  <!-- CSS Libraries -->
+  <link rel="stylesheet"  href="{{ asset('assets/modules/jqvmap/dist/jqvmap.min.css') }}">
+  <link rel="stylesheet"  href="{{ asset('assets/modules/weather-icon/css/weather-icons.min.css') }}">
+  <link rel="stylesheet"  href="{{ asset('assets/modules/weather-icon/css/weather-icons-wind.min.css') }}">
+  <link rel="stylesheet"  href="{{ asset('assets/modules/summernote/summernote-bs4.css') }}">
+
+    <!-- Template CSS -->
+    <link rel="stylesheet"  href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet"  href="{{ asset('assets/css/components.css') }}">
+    <!-- Start GA -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-94034622-3');
+    </script>
+    <!-- /END GA --></head>
+
+    <body>
+      <div id="app">
+        <div class="main-wrapper main-wrapper-1">
+          @include("partials.pages.dashboard.header-dashboard")
+          @include("partials.pages.dashboard.sidebar-dashboard")
+          @include("partials.pages.dashboard.content-dashboard")
+          {{-- @yield("content") --}}
+        </div>
+
+        <!-- General JS Scripts -->
+        <script src="{{ asset('assets/modules/jquery.min.js') }}"></script>
+        <script src="{{ asset('assets/modules/popper.js') }}"></script>
+        <script src="{{ asset('assets/modules/tooltip.js') }}"></script>
+        <script src="{{ asset('assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+        <script src="{{ asset('assets/modules/moment.min.js') }}"></script>
+        <script src="{{ asset('assets/js/stisla.js') }}"></script>
+
+        <!-- JS Libraies -->
+        <script src="{{ asset('assets/modules/simple-weather/jquery.simpleWeather.min.js') }}"></script>
+        <script src="{{ asset('assets/modules/chart.min.js') }}"></script>
+        <script src="{{ asset('assets/modules/jqvmap/dist/jquery.vmap.min.js') }}"></script>
+        <script src="{{ asset('assets/modules/jqvmap/dist/maps/jquery.vmap.world.js') }}"></script>
+        <script src="{{ asset('assets/modules/summernote/summernote-bs4.js') }}"></script>
+        <script src="{{ asset('assets/modules/chocolat/dist/js/jquery.chocolat.min.js') }}"></script>
+
+        <!-- Page Specific JS File -->
+        <script src="{{ asset('assets/js/page/index-0.js') }}"></script>
+
+        <!-- Template JS File -->
+        <script src="{{ asset('assets/js/scripts.js') }}"></script>
+        <script src="{{ asset('assets/js/custom.js') }}"></script>
+      </body>
+      </html>
---
+++ resources/views/partials/pages/dashboard/content-dashboard.blade.php
@@ -0,0 +1,117 @@
+<div class="main-content">
+  <section class="section">
+    <div class="section-header">
+      <h1>Dashboard</h1>
+    </div>
+    <div class="row">
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <div class="card card-statistic-1">
+          <div class="card-icon bg-primary">
+            <i class="far fa-user"></i>
+          </div>
+          <div class="card-wrap">
+            <div class="card-header">
+              <h4>Total Admin</h4>
+            </div>
+            <div class="card-body">
+              10
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <div class="card card-statistic-1">
+          <div class="card-icon bg-danger">
+            <i class="far fa-newspaper"></i>
+          </div>
+          <div class="card-wrap">
+            <div class="card-header">
+              <h4>News</h4>
+            </div>
+            <div class="card-body">
+              42
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <div class="card card-statistic-1">
+          <div class="card-icon bg-warning">
+            <i class="far fa-file"></i>
+          </div>
+          <div class="card-wrap">
+            <div class="card-header">
+              <h4>Reports</h4>
+            </div>
+            <div class="card-body">
+              1,201
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <div class="card card-statistic-1">
+          <div class="card-icon bg-success">
+            <i class="fas fa-circle"></i>
+          </div>
+          <div class="card-wrap">
+            <div class="card-header">
+              <h4>Online Users</h4>
+            </div>
+            <div class="card-body">
+              47
+            </div>
+          </div>
+        </div>
+      </div>                  
+    </div>
+    <div class="row">
+      <div class="col-lg-8 col-md-12 col-12 col-sm-12">
+        <div class="card">
+          <div class="card-header">
+            <h4>Statistics</h4>
+            <div class="card-header-action">
+              <div class="btn-group">
+                <a href="#" class="btn btn-primary">Week</a>
+                <a href="#" class="btn">Month</a>
+              </div>
+            </div>
+          </div>
+          <div class="card-body">
+            <canvas id="myChart" height="182"></canvas>
+            <div class="statistic-details mt-sm-4">
+              <div class="statistic-details-item">
+                <span class="text-muted"><span class="text-primary"><i class="fas fa-caret-up"></i></span> 7%</span>
+                <div class="detail-value">$243</div>
+                <div class="detail-name">Today's Sales</div>
+              </div>
+              <div class="statistic-details-item">
+                <span class="text-muted"><span class="text-danger"><i class="fas fa-caret-down"></i></span> 23%</span>
+                <div class="detail-value">$2,902</div>
+                <div class="detail-name">This Week's Sales</div>
+              </div>
+              <div class="statistic-details-item">
+                <span class="text-muted"><span class="text-primary"><i class="fas fa-caret-up"></i></span>9%</span>
+                <div class="detail-value">$12,821</div>
+                <div class="detail-name">This Month's Sales</div>
+              </div>
+              <div class="statistic-details-item">
+                <span class="text-muted"><span class="text-primary"><i class="fas fa-caret-up"></i></span> 19%</span>
+                <div class="detail-value">$92,142</div>
+                <div class="detail-name">This Year's Sales</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+  <footer class="main-footer">
+    <div class="footer-left">
+      Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+    </div>
+    <div class="footer-right">
+
+    </div>
+  </footer>
+</div>

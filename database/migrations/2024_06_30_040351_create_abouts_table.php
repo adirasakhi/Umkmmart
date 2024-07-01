67b4550f7ae0f1b75e927f4ba96e39fee48b9ff7
adirasakhi <mochadira14@gmail.com>
2024-07-01 13:07:53 +0700

    done

--- database/migrations/2024_06_30_040351_create_abouts_table.php
+++ database/migrations/2024_06_30_040351_create_abouts_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('abouts', function (Blueprint $table) {
             $table->id();
-            $table->text('content');
-            $table->string('image');
+            $table->text('content')->nullable();
+            $table->string('image')->nullable()->after('content');
             $table->timestamps();
         });
     }
